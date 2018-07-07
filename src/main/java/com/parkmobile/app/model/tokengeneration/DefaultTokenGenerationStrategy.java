d01405ed3461b19d606084ea5bb264ca79cb3b9a
Hugo Borges <hugobp14@hotmail.com>
2018-07-06 22:45:48 -0300

    Created a default strategy to create receipt tokens

---
+++ src/main/java/com/parkmobile/app/model/tokengeneration/DefaultTokenGenerationStrategy.java
@@ -0,0 +1,22 @@
+package com.parkmobile.app.model.tokengeneration;
+
+import java.sql.Timestamp;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import com.parkmobile.app.entity.Ticket;
+
+@Component
+@Qualifier("defaultTokenGeneration")
+public class DefaultTokenGenerationStrategy implements TokenGenerationStrategy {
+
+	@Override
+	public Ticket generateToken(Ticket ticket) {
+		
+		final Timestamp timestamp = Timestamp.valueOf(ticket.getTimeEntry());
+		ticket.setToken(timestamp.toString());
+		
+		return ticket;
+	}
+}
